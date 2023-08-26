f3aa43eb79d7a737df34ebdde06a36ab73112d7f
judy-chelangat <judysigilai75@gmail.com>
2023-08-26 18:01:15 +0300

    added comments and changed the name then used print statements to test the output

--- restuarant.py
+++ restuarant.py
@@ -1,6 +1,20 @@
+# Restaurant __init__()`
+# - Restaurants should be initialized with a name, as a string
+#- `Restaurant name()`
+# - returns the restaurant's name
+#- should not be able to change after the restaurant is created
+ 
+
 class Restuarant:
-    def __init__(self,r_name):
-        self.r_name=r_name
+    def __init__(self,restuarant_name): #initialized the restuarant with a name
+        self.restuarant_name=restuarant_name
 
     def name(self):
-        return self.r_name
+        return self.restuarant_name
+    
+
+restuarant1=Restuarant("Frangos")
+restuarant2=Restuarant("The Curve")
+
+print (restuarant1.name())
+print (restuarant2.name())
