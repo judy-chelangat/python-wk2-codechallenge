fc79dc6bff94f3d8b78d496adb656da11e01ab74
judy-chelangat <judysigilai75@gmail.com>
2023-08-26 18:35:02 +0300

    added comments and initialized the firstname and class methods

--- review.py
+++ review.py
@@ -6,15 +6,39 @@ from restuarant import Restuarant
  # - returns the rating for a restaurant.
 #-  `Review all()`
  # - returns all of the reviews
+
+
+ #- `Review customer()`
+ # - returns the customer object for that review
+  #- Once a review is created, should not be able to change the customer
+  # `Review restaurant()`
+ # - returns the restaurant object for that given review
+ # - Once a review is created, should not be able to change the restaurant
 class Review(Customer,Restuarant):
     all_reviews=[] #variable to hold all the reviews
-    def __init__(self,rating,first_name,last_name):
+    def __init__(self,first_name,restuarant_name,rating):
         self.rating = rating
-        super().__init__(first_name,last_name)
+        self.first_name=first_name
+        self.restuarant_name=restuarant_name
+        super().__init__(first_name,restuarant_name) #using super to initialize the name from the parent class
+        self.all_reviews.append(self)
+
     def review_rating(self):
         return self.rating
     
     @classmethod
     def review_all(cls):
         return cls.all_reviews
+    @classmethod
+    def review_customer(cls):
+        return 
+    def review_restuarant(cls):
+        return 
 
+customer1 = Customer("Judy", "sigilai")
+restuarant1=Restuarant("Frangos")
+
+review1=Review(customer1,restuarant1,6)
+print(review1.review_rating())
+
+print(Review.review_all())
