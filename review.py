c9d6dbad097b9ef1bd80e5179229354c07f6ab8b
judy-chelangat <judysigilai75@gmail.com>
2023-08-27 20:59:27 +0300

    removed the output test and moved it to main.py

--- review.py
+++ review.py
@@ -1,5 +1,4 @@
-from customer import Customer
-from restuarant import Restuarant
+
 #    Review __init__()`
  # - Reviews should be initialized with a customer, restaurant, and a rating (a number)
 #-  `Review rating()`
@@ -14,22 +13,22 @@ from restuarant import Restuarant
   # `Review restaurant()`
  # - returns the restaurant object for that given review
  # - Once a review is created, should not be able to change the restaurant
-class Review(Customer,Restuarant):
+class Review():
     all_reviews=[] #variable to hold all the reviews
     def __init__(self,first_name,restuarant_name,rating):
         self.rating = rating
         self.first_name=first_name
         self.restuarant_name=restuarant_name
-        super().__init__(first_name,restuarant_name) #using super to initialize the name from the parent class
+        # super().__init__(first_name,restuarant_name) #using super to initialize the name from the parent class
         self.all_reviews.append(self)
 
-    def review_rating(self):
+    def rating(self):
         return self.rating
     
-    def review_customer(self):
+    def customer(self):
         return self.first_name
     
-    def review_restuarant(self):
+    def restuarant(self):
         return self.restuarant_name
     
     @classmethod
@@ -37,14 +36,5 @@ class Review(Customer,Restuarant):
         return cls.all_reviews
    
    
+   
 
-customer1 = Customer("Judy", "sigilai")
-restuarant1=Restuarant("Frangos")
-
-review1=Review(customer1,restuarant1,6) #instance creation
-print(review1.review_rating()) #printing the rating
-
-print(Review.review_all())
-
-print(review1.review_customer().full_name())  # retrieving details from the customer class
-print(review1.review_restuarant().name()) 
