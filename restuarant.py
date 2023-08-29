beb8b1a4981f5cb45887f343bfa3b8a508033ceb
judy-chelangat <judysigilai75@gmail.com>
2023-08-29 09:52:55 +0300

    added a property

--- restuarant.py
+++ restuarant.py
@@ -12,11 +12,12 @@
 
 class Restuarant():
     def __init__(self,restuarant_name): #initialized the restuarant with a name
-        self.restuarant_name=restuarant_name
+        self._restuarant_name=restuarant_name
         self.reviews=[] #stores the reviews associated with the restuarant 
 
+    @property
     def name(self):
-        return self.restuarant_name
+        return self._restuarant_name
     
     def reviews(self): #returns a list of all the reviews 
         return self.reviews
@@ -34,5 +35,5 @@ class Restuarant():
 restuarant1=Restuarant("Frangos")
 restuarant2=Restuarant("The Curve")
 
-print (restuarant1.name())
-print (restuarant2.name())
+print (restuarant1.name)
+print (restuarant2.name)
